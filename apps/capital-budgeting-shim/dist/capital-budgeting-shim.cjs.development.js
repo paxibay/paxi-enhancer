4e2ebc88affef24649fe036d92a660c9a523be26
paynz <paynzhou@gmail.com>
2021-06-29 23:18:26 -0400

    adding apps

---
+++ apps/capital-budgeting-shim/dist/capital-budgeting-shim.cjs.development.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+var sum = function sum(a, b) {
+  // if ('development' === "development") {
+  //   console.log('boop');
+  // }
+  return a + b;
+};
+
+exports.sum = sum;
+//# sourceMappingURL=capital-budgeting-shim.cjs.development.js.map
